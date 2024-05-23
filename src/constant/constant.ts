bfba1a508f675c561e4459bfcaba7a43448d89f4
Agungdk56 <agungdwikurniyanto02@gmail.com>
2024-05-23 15:16:34 +0700

    adding constant file

---
+++ src/constant/constant.ts
@@ -0,0 +1,5 @@
+export const siteTitle = "Web and Mobile Developer";
+export const siteSeoTitle =
+  "Agung Dwi Kurniyanto, Web and Mobile Developer Portofolio";
+export const siteSeoDesc =
+  "Enthusiastic and innovative developer with a passion for creating user-friendly and efficient applications. Possesses a strong understanding of modern web and mobile development technologies. Proven ability to work independently and as part of a team to deliver high-quality projects on time and within budget";
